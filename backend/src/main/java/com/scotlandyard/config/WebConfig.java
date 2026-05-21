6ac060fcbc1955f6206d6314c58226988fd270ce
Ming Bao <mingbao2891@gmail.com>
2026-05-22 00:19:12 +1200

    fixed a warning

--- backend/src/main/java/com/scotlandyard/config/WebConfig.java
+++ backend/src/main/java/com/scotlandyard/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.scotlandyard.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(@NonNull CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
